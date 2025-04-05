d0c25815306b896b0fb65bae06f4dafd5749c2bf
Kiki Mahesta <kimsta.ti@gmail.com>
2025-04-05 14:56:00 +0700

    perbaikan topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -19,20 +19,21 @@ const Topbar = () => {
       {/* Kontainer utama: pakai h-8 agar tinggi tetap */}
       <div className="container mx-auto px-6 flex items-center h-8 space-x-4 overflow-hidden">
         {/* Tanggal Otomatis */}
-        <span className="font-semibold whitespace-nowrap leading-none">
-          {today}
-        </span>
+        <span className="font-semibold whitespace-nowrap leading-none text-xs md:text-sm">
+  {today}
+</span>
 
-        {/* Label "Kajian Populer" isi penuh tanpa jarak atas-bawah */}
+
+        {/* Label "Kajian Populer" - teks disembunyikan di mobile */}
         <div className="bg-green-600 flex items-center h-full px-2 space-x-1 whitespace-nowrap">
           <i className="ri-book-open-fill"></i>
-          <span className="leading-none">Kajian Populer</span>
+          <span className="leading-none hidden md:inline">Kajian Populer</span>
         </div>
 
         {/* Marquee Pemateri Kajian */}
         <div className="relative flex-1 h-5 overflow-hidden">
           <div className="absolute whitespace-nowrap animate-marquee text-gray-500">
-          Dosa-dosa yang membinasakan ~  Ad Durus Al Muhimmah Li' Ammatil Ummah ~  Ust. Abu Umar Raih Gelar, Lc. 
+            Dosa-dosa yang membinasakan ~ Ad Durus Al Muhimmah Li' Ammatil Ummah ~ Ust. Abu Umar Raih Gelar, Lc.
           </div>
         </div>
       </div>
@@ -41,5 +42,3 @@ const Topbar = () => {
 };
 
 export default Topbar;
-
-
